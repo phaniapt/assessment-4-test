c04a47c12d334fece962f209ecac6b86b7ed400d
phani476 <ppasumarthi@osius.com>
2023-07-03 18:03:00 +0530

    jenkins new commit2

--- provider.tf
+++ provider.tf
@@ -5,10 +5,11 @@ terraform {
       version = "5.5.0"
     }
   }
+#   required_version = "1.4.6"
 }
 
 provider "aws" {
-    region     = var.aws_region
-    access_key = "${{access_key}}"
-    secret_key = "${{secret_key}}"
+    region = var.aws_region
+    # access_key = ${{access_key}}
+    # secret_key = ${{secret_key}}
 }
